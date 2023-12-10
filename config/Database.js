cd9f272533f905d3bf0afdeb434404afebccb8f9
sandhydhannova <sandhydhannova01@gmail.com>
2023-12-10 22:43:48 +0700

    finalisasi

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import mysql2 from "mysql2";
 
 const db = new Sequelize("vpuijmph_group_15", "vpuijmph_fiftyhotels", "8xy8@uF]g;jX", {
-  host: "hydroflow.id",
+  host: "be-2-surabaya-15-4ojquiofc-sandhy-dhannovas-projects.vercel.app",
   dialect: "mysql",
   dialectModule: mysql2
 });
